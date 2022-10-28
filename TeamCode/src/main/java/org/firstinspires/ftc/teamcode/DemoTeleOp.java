a07887ec6937a643ff04664203f60a187464bfee
javaLikeItsHot <114178481+javaLikeItsHot@users.noreply.github.com>
2022-10-28 11:25:16 -0500

    Fix Download Issues
    Added in @Teleop above class declaration which is needed to have the op mode appear on the driver station. It now runs.
    
    Co-Authored-By: Victoria Mercouris <14935998+vnmercouris@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoTeleOp.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+@TeleOp(name="DemoTeleOp", group="Linear Opmode")
 public class DemoTeleOp extends LinearOpMode{
 
     private ElapsedTime runtime = new ElapsedTime();
@@ -10,10 +12,19 @@ public class DemoTeleOp extends LinearOpMode{
 
     @Override
     public void runOpMode(){
-
         robot.init(hardwareMap);
 
-        robot.drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.left_trigger, gamepad1.right_trigger);
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+
+
+            robot.drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.left_trigger, gamepad1.right_trigger);
+        }
     }
 
 }
