ec89f4c31f0d2a81e76cb1e7ba5411af0285e69b
caitlinestrada23 <caitlinestrada23@newmanschool.org>
2022-10-29 13:23:42 -0500

    Revert "Fix Download Issues"
    This reverts commit a07887ec6937a643ff04664203f60a187464bfee.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoTeleOp.java
@@ -1,10 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="DemoTeleOp", group="Linear Opmode")
 public class DemoTeleOp extends LinearOpMode{
 
     private ElapsedTime runtime = new ElapsedTime();
@@ -12,19 +10,10 @@ public class DemoTeleOp extends LinearOpMode{
 
     @Override
     public void runOpMode(){
+
         robot.init(hardwareMap);
 
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-
-
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-
-
-
-            robot.drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.left_trigger, gamepad1.right_trigger);
-        }
+        robot.drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.left_trigger, gamepad1.right_trigger);
     }
 
 }
