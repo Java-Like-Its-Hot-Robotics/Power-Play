dcb3f9633cd58b369820267167822b1fdea90fbb
Cajunvoodoo <tomdwy1008@gmail.com>
2023-01-10 22:26:13 -0600

    Start creation of CustomOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/CustomOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/CustomOpMode.java
@@ -1,5 +1,18 @@
 package org.firstinspires.ftc.teamcode.api;
 
-public class CustomOpMode extends OpMode implements Gamepad.GamepadCallback {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpModeManagerNotifier.Notifications;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
+public class CustomOpMode extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
