db5ae2437692db6cc93ae94aba1d928b8c7103d1
Cajunvoodoo <tomdwy1008@gmail.com>
2023-01-10 22:25:58 -0600

    ControllerManager abstraction initial commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/ControllerManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/ControllerManager.java
@@ -1,31 +1,63 @@
+package org.firstinspires.ftc.teamcode.api;
+
+import com.google.common.collect.Multimap;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import static org.firstinspires.ftc.teamcode.api.ControllerKey.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public abstract class ControllerManager {
+    private Multimap<KeyEvent, Binding> bindings;
+    private Gamepad gamepad;
 
-    private DriveManager dm;
-    public ControllerManager (DriveManager dm) {
-        this.dm = dm;
+    public ControllerManager bind(Binding binding, KeyEvent key) {
+        bindings.put(key, binding);
+        return this;
+    }
+    public void unbind(Binding binding, KeyEvent key) {
+        bindings.remove(key, binding);
+    }
+    public void check() {
+        Iterator<KeyEvent> keyEventIterator = bindings.keys().iterator();
+
+        //For each key event, call all of its bound handlers with the key event in question and
+        //the gamepad state
+        while(keyEventIterator.hasNext()) {
+            KeyEvent nextKeyEvent = keyEventIterator.next();
+            Iterator<Binding> bindingIterator = bindings.get(nextKeyEvent).iterator();
+            while(bindingIterator.hasNext()) {
+                Binding nextBinding = bindingIterator.next();
+                nextBinding.handleKey(nextKeyEvent, gamepad);
+            }
+
+        }
     }
 
-    private ControllerManager () {}
-
-    public void manage() {
-        //TODO FIXME
+    public int getGamepadId() {
+        return gamepad.getGamepadId();
     }
 
-    abstract void xButton();
-    abstract void yButton();
-    abstract void aButton();
-    abstract void bButton();
-    abstract void leftTrigger();
-    abstract void rightTrigger();
-    abstract void leftStick();
-    abstract void rightStick();
-    abstract void leftStickIn();
-    abstract void rightStickIn();
-    abstract void dpadUp();
-    abstract void dpadDown();
-    abstract void dpadLeft();
-    abstract void dpadRight();
-    abstract void leftBumper();
-    abstract void rightBumper();
+
+//    private List<KeyEvent> gamepadToEvents() {
+//        List<KeyEvent> events = new ArrayList<>();
+//        events.add(new AnalogKeyEvent(LS));
+//        events.add(new AnalogKeyEvent(RS));
+//        events.add(new DigitalKeyEvent(A));
+//        events.add(new DigitalKeyEvent(B));
+//        events.add(new DigitalKeyEvent(X));
+//        events.add(new DigitalKeyEvent(Y));
+//        events.add(new DigitalKeyEvent(RS));
+//        events.add(new DigitalKeyEvent(LS));
+//        events.add(new DigitalKeyEvent(LS_IN));
+//        events.add(new DigitalKeyEvent(RS_IN, gamepad.right_stick_button));
+//        events.add(new DigitalKeyEvent(DOWN,  gamepad.dpad_down));
+//        events.add(new DigitalKeyEvent(LEFT,  gamepad.dpad_left));
+//        events.add(new DigitalKeyEvent(UP,    gamepad.dpad_up));
+//        events.add(new DigitalKeyEvent(RIGHT, gamepad.dpad_right));
+//        return events;
+//    }
 }
